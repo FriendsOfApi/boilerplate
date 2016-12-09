991adc95fc587bcdf9787314dd9ff4bfca60968e
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-12-09 23:05:46 +0000

    Apply fixes from StyleCI

--- src/Resource/Api/Tweet/Tweet.php
+++ src/Resource/Api/Tweet/Tweet.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 namespace APIPHP\Boilerplate\Resource\Api\Tweet;
 
 /**
