92358ba2664b1f868a26b7be9b2fb55351fc5c6d
sagikazarmark <mark.sagikazar@gmail.com>
2016-12-28 17:56:49 +0000

    Apply fixes from StyleCI

--- src/Api/HttpApi.php
+++ src/Api/HttpApi.php
@@ -2,7 +2,7 @@
 
 /*
  * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
+ * of the MIT license. See the LICENSE file for details.
  */
 
 namespace FAPI\Boilerplate\Api;
