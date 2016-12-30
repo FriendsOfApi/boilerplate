7866303369324ca22f6a11bbd3a9d33a234c6540
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-12-30 09:54:12 +0100

    style fix

--- tests/HttpClientConfiguratorTest.php
+++ tests/HttpClientConfiguratorTest.php
@@ -1,6 +1,11 @@
 <?php
 
-declare (strict_types=1);
+declare(strict_types=1);
+
+/*
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
 
 namespace FAPI\boilerplate\tests;
 
